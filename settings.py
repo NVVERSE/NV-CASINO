39e61ab04ab72542c6c886ea804c251d4bf8aa3b
NVVERSE <molupandey3998@gmail.com>
2026-09-24 17:01:18 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,8 @@
-BOT_TOKEN = "8628180136:AAFWY3LDxM01xIV4hr19JNOXWcnc7cEJ8iQ"
+BOT_TOKEN = "8728557922:AAH2GqE6nqtF1Mm_-GT5Fq6T5I2r5CykHic
+"
 ADMINS = [7995159553]  # आपकी एडमिन आईडी ब्रैकेट के अंदर आ गई
 TECH_SUPPORT_USER_ID = 7995159553
-UPI_ID = "shudhanshu539@slc"  
+UPI_ID = "shudhanshu539@slc"
+
+
+
