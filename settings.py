57a98ef64e1871549cada54557fcecb28d76b9e6
NVVERSE <molupandey3998@gmail.com>
2026-09-24 17:10:53 +0530

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-BOT_TOKEN = "8728557922:AAH2GqE6nqtF1Mm_-GT5Fq6T5I2r5CykHic
-"
-ADMINS = [7995159553]  # आपकी एडमिन आईडी ब्रैकेट के अंदर आ गई
-TECH_SUPPORT_USER_ID = 7995159553
-UPI_ID = "shudhanshu539@slc"
-
-
-
